a4161969b1e1f21ff95a98eb1cf96a6789fff5e0
Alex Shapalov <shapalov@gmail.com>
2026-04-02 14:27:31 -0700

    feat(cors): add CORS support for replay resources and static files
    - Introduced new CORS rules to allow GET and OPTIONS methods for the /replay/* endpoint, rrweb.min.js, and rrweb.min.css resources.
    - This enhancement facilitates cross-origin requests for replay functionality and related assets.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,5 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
     resource "/api/*", headers: :any, methods: [:post, :options]
+    resource "/replay/*", headers: :any, methods: [:get, :options]
+    resource "/rrweb.min.js", headers: :any, methods: [:get, :options]
+    resource "/rrweb.min.css", headers: :any, methods: [:get, :options]
   end
 end
